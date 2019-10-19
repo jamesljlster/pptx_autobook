f85f3c54d46b030d7d866fc941f68f90e7e89047
Zheng-Ling Lai <jamesljlster@gmail.com>
2019-10-19 23:31:04 +0800

    Add get slide id and index mapping function

--- pptx_proc.py
+++ pptx_proc.py
@@ -1,7 +1,27 @@
-import uuid
-
 from win32com.client import Dispatch, constants
 from os.path import realpath
+from lxml import etree
+
+
+def pptx_slide_get_id_index_map(pptXml):
+
+    sldIdMap = {}
+
+    # Get slide list node
+    sldRoot = None
+    ret = pptXml.xpath('/p:presentation/p:sldIdLst',
+                       namespaces=pptXml.nsmap)
+    if len(ret) != 1:
+        raise RuntimeError('Broken PPTX file?!')
+    else:
+        sldRoot = ret[0]
+
+    # Get slide id list
+    sldIdLst = sldRoot.findall('.//p:sldId', namespaces=pptXml.nsmap)
+    for i in range(len(sldIdLst)):
+        sldIdMap[sldIdLst[i].get('id')] = i + 1
+
+    return sldIdMap
 
 
 def pptx_slide_export(pptPath, imgDir, format):
@@ -23,5 +43,14 @@ def pptx_slide_export(pptPath, imgDir, format):
 # Test
 if __name__ == '__main__':
 
-    pptx_slide_export('./pptx_file/test.pptx',
-                      './pptx_file/' + str(uuid.uuid4()), 'PNG')
+    import uuid
+
+    from zipfile import ZipFile
+
+    fPath = './pptx_file/test.pptx'
+
+    zFile = ZipFile(fPath)
+    pptXml = etree.fromstring(zFile.read('ppt/presentation.xml'))
+    print('Slide ID Map:', pptx_slide_get_id_index_map(pptXml))
+
+    pptx_slide_export(fPath, './pptx_file/' + str(uuid.uuid4()), 'PNG')
