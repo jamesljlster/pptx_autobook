6b65c5af049bbe616094cb39df25b987b61a9ad7
Zheng-Ling Lai <jamesljlster@gmail.com>
2019-10-20 10:28:50 +0800

    First release?!

--- __main__.py
+++ __main__.py
@@ -4,15 +4,17 @@ import os
 
 if __package__ == 'pptx_autobook':
     from pptx_autobook.pptx_proc import pptx_get_outline, pptx_slide_export
+    from pptx_autobook.docx_proc import docx_autobook
     from pptx_autobook.arg_parse import get_arg
 else:
     from pptx_proc import pptx_get_outline, pptx_slide_export
+    from docx_proc import docx_autobook
     from arg_parse import get_arg
 
 if __name__ == '__main__':
 
     # Get program options
     args = get_arg()
-    print(args)
 
-    print('Hello World')
+    # Autobook!!!
+    docx_autobook(args.pptx_src, args.docx_in, args.docx_out)
--- arg_parse.py
+++ arg_parse.py
@@ -4,8 +4,11 @@ import argparse as argp
 def get_arg():
     parser = argp.ArgumentParser()
 
-    parser.add_argument('--pptx-src', type=str, help='PPTX source file path')
-    parser.add_argument('--docx-out', type=str,
+    parser.add_argument('--pptx-src', type=str,
+                        required=True, help='PPTX source file path')
+    parser.add_argument('--docx-in', type=str, default=None,
+                        help='DOCX input file path')
+    parser.add_argument('--docx-out', type=str, required=True,
                         help='DOCX output file path. Overwrite if file is existed')
 
     args = parser.parse_args()
